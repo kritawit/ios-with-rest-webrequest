f8f13d2ee8a36142f93265eb6cc7ae668899dfae
Header-Develop <kritawitbunket@gmail.com>
2017-02-09 20:55:41 +0700

    Initial Commit

---
+++ rainyshinycloudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainyshinycloudy
+//
+//  Created by Header-Develop on 2/9/2560 BE.
+//  Copyright © 2560 Header-Devs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
